c375d81d464ede6e659e13f2a187701e0e570b25
shashank.rawat <aquib.khan@fonantrix.com>
2016-09-28 02:24:22 -0400

    Minor changes

--- WebContent/app/components/ngAutocomplete.directive.js
+++ WebContent/app/components/ngAutocomplete.directive.js
@@ -1,38 +1,5 @@
 'use strict';
 
-/**
- * A directive for adding google places autocomplete to a text box
- * google places autocomplete info: https://developers.google.com/maps/documentation/javascript/places
- *
- * Simple Usage:
- *
- * <input type="text" ng-autocomplete="result"/>
- *
- * creates the autocomplete text box and gives you access to the result
- *
- *   + `ng-autocomplete="result"`: specifies the directive, $scope.result will hold the textbox result
- *
- *
- * Advanced Usage:
- *
- * <input type="text" ng-autocomplete="result" details="details" options="options"/>
- *
- *   + `ng-autocomplete="result"`: specifies the directive, $scope.result will hold the textbox autocomplete result
- *
- *   + `details="details"`: $scope.details will hold the autocomplete's more detailed result; latlng. address components, etc.
- *
- *   + `options="options"`: options provided by the user that filter the autocomplete results
- *
- *      + options = {
- *           types: type,        string, values can be 'geocode', 'establishment', '(regions)', or '(cities)'
- *           bounds: bounds,     google maps LatLngBounds Object
- *           country: country    string, ISO 3166-1 Alpha-2 compatible country code. examples; 'ca', 'us', 'gb'
- *         }
- *
- *
- */
-
-
 angular.module( "ngAutocomplete", [])
 	.directive('ngAutocomplete', function($parse) {
     return {
